767513e0bc45cb3c7d2076249406b5ebe2418658
TopKek <braebear85@gmail.com>
2018-02-20 19:58:48 -0500

    Fixed more bugs aaaa
    Fixed a bug that caused status to become [object, Object] before showing the stop message. Also added a check to see if anything was playing when the client is started.

--- discord_frienly.js
+++ discord_frienly.js
@@ -6,7 +6,6 @@ const interfaceName = "org.mpris.MediaPlayer2.Player";
 const objectPath = "/org/mpris/MediaPlayer2";
 const propIfaceName = "org.freedesktop.DBus.Properties"
 var metadata;
-var pos;
 var savedArtist;
 var savedTitle;
 
@@ -27,7 +26,7 @@ service.getInterface(objectPath, interfaceName, (err, iface) => {
     );
     process.exit(1);
   }
-  
+  console.log("Connected");
   iface.Metadata((err, str) => {
     if (err) {
       console.error(`Error while calling Metadata: ${err}`);
@@ -40,16 +39,27 @@ service.getInterface(objectPath, interfaceName, (err, iface) => {
       artist = metadata.find(function(element) {
         return element.toString().startsWith("xesam:artist,[object Object],");
       });
-      savedArtist = artist.toString().replace("xesam:artist,[object Object],", "");
-      savedTitle = title.toString().replace("xesam:title,[object Object],", "");
-      console.log(title + artist);
-      client.updatePresence({
-        state: artist.toString().replace("xesam:artist,[object Object],", ""),
-        details: title.toString().replace("xesam:title,[object Object],", ""),
-        largeImageKey: "rhythmbox",
-        smallImageKey: "play",
-        instance: true,
-      });
+      if (!title && !artist) {
+        client.updatePresence({
+          state: "By TopKek - https://github.com/ToppleKek/rhythmbox-discord",
+          details: "Rhythmbox RPC",
+          largeImageKey: "rhythmbox",
+          smallImageKey: "stop",
+          instance: true,
+        });        
+      }
+      else {
+        savedArtist = artist.toString().replace("xesam:artist,[object Object],", "");
+        savedTitle = title.toString().replace("xesam:title,[object Object],", "");
+        console.log(title + artist);
+        client.updatePresence({
+          state: artist.toString().replace("xesam:artist,[object Object],", ""),
+          details: title.toString().replace("xesam:title,[object Object],", ""),
+          largeImageKey: "rhythmbox",
+          smallImageKey: "play",
+          instance: true,
+        });
+      }
     }
   });
 });
@@ -67,79 +77,91 @@ service.getInterface(objectPath, propIfaceName, (err, iface) => {
   }
   
   iface.on("PropertiesChanged", function(msg, data) {
-    meta = data.find(function(element) {
+    var meta = data.find(function(element) {
       return element.toString().startsWith("Metadata,[object Object],");
     });
     var status;
-    if (!meta) {
-      console.log("DEBUG: data is: " + data.toString());
-      if (data.toString().includes("PlaybackStatus,[object Object],")) {
-        console.log("Change, but metadata was not updated");
-        service.getInterface(objectPath, interfaceName, (err, iface) => {
-          iface.PlaybackStatus((err, str) => {
-            console.log(str);
-            switch (str) {
-              case "Playing":
-                console.log("WE ARE PLAYING");
-                client.updatePresence({
-                  state: savedArtist,
-                  details: savedTitle,
-                  largeImageKey: "rhythmbox",
-                  smallImageKey: "pause",
-                  instance: true,
-                });    
-                break;            
-              case "Paused":
-                console.log("WE ARE PAUSED");
-                client.updatePresence({
-                  state: savedArtist,
-                  details: savedTitle,
-                  largeImageKey: "rhythmbox",
-                  smallImageKey: "pause",
-                  instance: true,
-                });    
-                break;
-              case "Stopped":
-                console.log("WE ARE STOPPED");
-                client.updatePresence({
-                  state: "By TopKek",
-                  details: "Rhythmbox RPC",
-                  largeImageKey: "rhythmbox",
-                  smallImageKey: "stop",
-                  instance: true,
-                });    
-                break;
-            }
-          });
-        });
-      }
+    service.getInterface(objectPath, interfaceName, (err, iface) => {
+      iface.PlaybackStatus((err, str) => {
+        if (!meta) {
 
-      else {
-        console.log("Change, but playbackstatus was not updated");
-      }
-    }
-    else {
-      var metaStr = meta[1].toString();
-      console.log("---META---\n" + meta[1] + "\n---END META---");
-      title = metaStr.substring(metaStr.indexOf(",xesam:title,[object Object],") + 29, metaStr.indexOf(",xesam:artist,"));
-      console.log("title: " + title);
-      artist = metaStr.substring(metaStr.indexOf(",xesam:artist,[object Object],") + 30, metaStr.indexOf(",xesam:album,"));
-      console.log("artist: " + artist);
-      if (!title || !artist) {
-        console.log("Change, but title and artist was not changed.");
-      }
-      else {
-        savedArtist = artist;
-        savedTitle = title;
-        client.updatePresence({
-          state: artist,
-          details: title,
-          largeImageKey: "rhythmbox",
-          smallImageKey: "play",
-          instance: true,
-        });      
-      }
-    }
+          console.log("DEBUG: data is: " + data.toString());
+            if (!data.toString().includes("PlaybackStatus,[object Object],") && str === "Stopped") {
+              console.log("WE ARE STOPPED (Before checking playbackstats)");
+              client.updatePresence({
+                state: "By TopKek - https://github.com/ToppleKek/rhythmbox-discord",
+                details: "Rhythmbox RPC",
+                largeImageKey: "rhythmbox",
+                smallImageKey: "stop",
+                instance: true,
+              });  
+            }
+            if (data.toString().includes("PlaybackStatus,[object Object],")) {
+              console.log("Change, but metadata was not updated");
+              console.log(str);
+              switch (str) {
+                case "Playing":
+                  console.log("WE ARE PLAYING");
+                  client.updatePresence({
+                    state: savedArtist,
+                    details: savedTitle,
+                    largeImageKey: "rhythmbox",
+                    smallImageKey: "play",
+                    instance: true,
+                  });    
+                  break;            
+                case "Paused":
+                  console.log("WE ARE PAUSED");
+                  client.updatePresence({
+                    state: savedArtist,
+                    details: savedTitle,
+                    largeImageKey: "rhythmbox",
+                    smallImageKey: "pause",
+                    instance: true,
+                  });    
+                  break;
+                case "Stopped":
+                  console.log("WE ARE STOPPED");
+                  client.updatePresence({
+                    state: "By TopKek - https://github.com/ToppleKek/rhythmbox-discord",
+                    details: "Rhythmbox RPC",
+                    largeImageKey: "rhythmbox",
+                    smallImageKey: "stop",
+                    instance: true,
+                  });    
+                  break;
+              }
+            }
+            else {
+              console.log("Change, but playbackstatus was not updated");
+            }
+        }
+        else {    
+          var metaStr = meta[1].toString();
+          console.log("---META---\n" + meta[1] + "\n---END META---");
+          title = metaStr.substring(metaStr.indexOf(",xesam:title,[object Object],") + 29, metaStr.indexOf(",xesam:artist,"));
+          console.log("title: " + title);
+          artist = metaStr.substring(metaStr.indexOf(",xesam:artist,[object Object],") + 30, metaStr.indexOf(",xesam:album,"));
+          console.log("artist: " + artist);
+          if (!title || !artist) {
+            console.log("Change, but title and artist was not changed.");
+          }
+          else {
+            if (str !== "Stopped") {
+              savedArtist = artist;
+              savedTitle = title;
+              client.updatePresence({
+                state: artist,
+                details: title,
+                largeImageKey: "rhythmbox",
+                smallImageKey: "play",
+                instance: true,
+              });
+            }      
+          }
+        }
+      });
+    });
   });  
 });
 
