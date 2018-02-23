309e99569dce757599f49e8e15789abfda5e908b
TopKek <braebear85@gmail.com>
2018-02-22 21:26:42 -0500

    Added time elapsed and img text
    Pls tell me if time elapsed should be the whole listening time (the way it is right now) or per song

--- discord_frienly.js
+++ discord_frienly.js
@@ -4,10 +4,13 @@ const sessionBus = dbus.sessionBus();
 const serviceName = "org.mpris.MediaPlayer2.rhythmbox";
 const interfaceName = "org.mpris.MediaPlayer2.Player";
 const objectPath = "/org/mpris/MediaPlayer2";
-const propIfaceName = "org.freedesktop.DBus.Properties"
+const propIfaceName = "org.freedesktop.DBus.Properties";
+const playlistIfaceName = "org.mpris.MediaPlayer2.Playlists";
 var metadata;
 var savedAlbum;
 var savedDetails;
+var savedLargeImgText;
+var timestamp = Math.floor(Date.now() / 1000);
 
 if (!sessionBus) {
   throw new Error("Could not connect to the DBus session bus.");
@@ -42,28 +45,44 @@ service.getInterface(objectPath, interfaceName, (err, iface) => {
       album = metadata.find(function(element) {
         return element.toString().startsWith("xesam:album,[object Object],");
       });
-      if (!title && !artist && !album) {
-        client.updatePresence({
-          state: "By TopKek - https://github.com/ToppleKek/rhythmbox-discord",
-          details: "Rhythmbox RPC",
-          largeImageKey: "rhythmbox",
-          smallImageKey: "stop",
-          instance: true,
-        });        
-      }
-      else {
-        savedAlbum = album.toString().replace("xesam:album,[object Object],", "").substring(0, 128);
-        var details = title.toString().replace("xesam:title,[object Object],", "") + " - " + artist.toString().replace("xesam:artist,[object Object],", "");
-        savedDetails = details.substring(0, 128);
-        console.log(title + artist + album);
-        client.updatePresence({
-          state: album.toString().replace("xesam:album,[object Object],", "").substring(0, 128),
-          details: details.substring(0, 128),
-          largeImageKey: "rhythmbox",
-          smallImageKey: "play",
-          instance: true,
-        });
-      }
+      service.getInterface(objectPath, playlistIfaceName, (err, iface) => {
+        iface.ActivePlaylist((err, str) => {
+          if (str) {
+            var largeImgText = "Playlist: " + str[1].toString().split(",")[1];
+            savedLargeImgText = largeImgText;
+            console.log(str[1].toString().split(",")[1]);
+          }
+          else {
+            console.log("No playlist");        
+          }
+         if (!title && !artist && !album) {
+           client.updatePresence({
+             state: "By TopKek - https://github.com/ToppleKek/rhythmbox-discord",
+             details: "Rhythmbox RPC",
+             largeImageKey: "rhythmbox",
+             smallImageKey: "stop",
+             smallImageText: "Stopped",
+             instance: true,
+           });        
+         }
+         else {
+           savedAlbum = album.toString().replace("xesam:album,[object Object],", "").substring(0, 128);
+           var details = title.toString().replace("xesam:title,[object Object],", "") + " - " + artist.toString().replace("xesam:artist,[object Object],", "");
+           savedDetails = details.substring(0, 128);
+           console.log(title + artist + album);
+           client.updatePresence({
+             state: album.toString().replace("xesam:album,[object Object],", "").substring(0, 128),
+             details: details.substring(0, 128),
+             startTimestamp: timestamp,
+             largeImageKey: "rhythmbox",
+             largeImageText: largeImgText,
+             smallImageKey: "play",
+             smallImageText: "Playing",
+             instance: true,
+           });
+         }
+       });
+      });
     }
   });
 });
@@ -97,6 +116,7 @@ service.getInterface(objectPath, propIfaceName, (err, iface) => {
                 details: "Rhythmbox RPC",
                 largeImageKey: "rhythmbox",
                 smallImageKey: "stop",
+                smallImageText: "Stopped",
                 instance: true,
               });  
             }
@@ -109,8 +129,11 @@ service.getInterface(objectPath, propIfaceName, (err, iface) => {
                   client.updatePresence({
                     state: savedAlbum,
                     details: savedDetails,
+                    startTimestamp: timestamp,
                     largeImageKey: "rhythmbox",
+                    largeImageText: savedLargeImgText,
                     smallImageKey: "play",
+                    smallImageText: "Playing",
                     instance: true,
                   });    
                   break;            
@@ -119,8 +142,11 @@ service.getInterface(objectPath, propIfaceName, (err, iface) => {
                   client.updatePresence({
                     state: savedAlbum,
                     details: savedDetails,
+                    startTimestamp: timestamp,
                     largeImageKey: "rhythmbox",
+                    largeImageText: savedLargeImgText,
                     smallImageKey: "pause",
+                    smallImageText: "Paused",
                     instance: true,
                   });    
                   break;
@@ -131,6 +157,7 @@ service.getInterface(objectPath, propIfaceName, (err, iface) => {
                     details: "Rhythmbox RPC",
                     largeImageKey: "rhythmbox",
                     smallImageKey: "stop",
+                    smallImageText: "Stopped",
                     instance: true,
                   });    
                   break;
@@ -140,35 +167,49 @@ service.getInterface(objectPath, propIfaceName, (err, iface) => {
               console.log("Change, but playbackstatus was not updated");
             }
         }
-        else {    
-          var metaStr = meta[1].toString();
-          console.log("---META---\n" + meta[1] + "\n---END META---");
-          title = metaStr.substring(metaStr.indexOf(",xesam:title,[object Object],") + 29, metaStr.indexOf(",xesam:artist,"));
-          console.log("title: " + title);
-          artist = metaStr.substring(metaStr.indexOf(",xesam:artist,[object Object],") + 30, metaStr.indexOf(",xesam:album,"));
-          console.log("artist: " + artist);
-          album = metaStr.substring(metaStr.indexOf(",xesam:album,[object Object],") + 29, metaStr.indexOf(",xesam:genre,"));
-          console.log("album: " + album);
-          var details = title.substring(0, 128) + " - " + artist.substring(0, 128);
-          if (!title || !artist || !album) {
-            console.log("Change, but title and artist was not changed.");
-          }
-          else {
-            if (str !== "Stopped") {
-              savedDetails = details.substring(0, 128);
-              savedAlbum = album.substring(0, 128);
-              client.updatePresence({
-                state: album.substring(0, 128),
-                details: details.substring(0, 128),
-                largeImageKey: "rhythmbox",
-                smallImageKey: "play",
-                instance: true,
-              });
-            }      
-          }
+        else {
+          service.getInterface(objectPath, playlistIfaceName, (err, iface) => {
+            iface.ActivePlaylist((err, playStr) => {
+              if (playStr) {
+                var largeImgText = "Playlist: " + playStr[1].toString().split(",")[1];
+                savedLargeImgText = largeImgText;
+                console.log(playStr[1].toString().split(",")[1]);
+              }
+              else {
+               console.log("No playlist");        
+              }
+              var metaStr = meta[1].toString();
+              console.log("---META---\n" + meta[1] + "\n---END META---");
+              title = metaStr.substring(metaStr.indexOf(",xesam:title,[object Object],") + 29, metaStr.indexOf(",xesam:artist,"));
+              console.log("title: " + title);
+              artist = metaStr.substring(metaStr.indexOf(",xesam:artist,[object Object],") + 30, metaStr.indexOf(",xesam:album,"));
+              console.log("artist: " + artist);
+              album = metaStr.substring(metaStr.indexOf(",xesam:album,[object Object],") + 29, metaStr.indexOf(",xesam:genre,"));
+              console.log("album: " + album);
+              var details = title.substring(0, 128) + " - " + artist.substring(0, 128);
+              if (!title || !artist || !album) {
+                console.log("Change, but title and artist was not changed.");
+              }
+              else {
+                if (str !== "Stopped") {
+                  savedDetails = details.substring(0, 128);
+                  savedAlbum = album.substring(0, 128);
+                  client.updatePresence({
+                    state: album.substring(0, 128),
+                    details: details.substring(0, 128),
+                    startTimestamp: timestamp,
+                    largeImageKey: "rhythmbox",
+                    largeImageText: largeImgText,
+                    smallImageKey: "play",
+                    smallImageText: "Playing",
+                    instance: true,
+                  });
+                }      
+              }
+            });
+          });
         }
-      });
+      });  
     });
-  });  
+  });
 });
-
